84bd8e96c9dfe679c4e5a25cdcfb16ad7f4c62c5
ahavrius <asta@pop-os.localdomain>
2019-03-14 09:13:06 +0200

    1 task + 2 task

---
+++ lab12.R
@@ -0,0 +1,77 @@
+library(triangle)
+
+alpha = 0.05
+color = 1
+tt = seq(0, 2, 0.01)
+
+
+#part 2
+FKM = function(x, sample, delta){
+  dot = 1
+  n = length(sample)
+  index = order(sample)
+  sample = sample[index]
+  delta = delta[index]
+  i = 1
+  while (i <= n && sample[i] <= x)
+  {
+    dot = dot * (1 - delta[i]/(n - i + 1))
+    i = i + 1  
+  }
+  1 - dot
+}
+
+DFsqrt = function(x, sample,  delta)
+{
+  n = length(sample)
+  index = order(sample)
+  sample = sample[index]
+  delta = delta[index]
+  s = 0
+  for (i in 1:n)
+    if ((n + 1 - i - delta[i]) != 0)
+      s = s + (delta[i] * (sample[i] <= x)) / ((n + 1 - i)*(n + 1 - i - delta[i]))
+    (1 - FKM(x, sample, delta))*sqrt(s)
+}
+  
+Fconf = function(x, sample, delta, alpha)
+{
+  lambda = qnorm(1 - alpha/2)
+  Fnx = FKM(x, sample, delta)
+  stand = sqrt(DFsqrt(x, sample, delta) / length(sample)) * lambda
+  stand
+  c(-stand + Fnx, stand + Fnx)
+}
+
+
+x_value =qtriangle(1/2, 0, 2)
+A_freq = matrix(ncol = 2, nrow = 0)
+matplot(tt, pmin(ptriangle(tt, 0, 2), punif(tt, 0, 2)), col = color, type = "s")
+for (n in c(5, 10, 100, 500, 1000))
+{
+  color = color + 1
+  F = rtriangle(n, 0, 2)
+  G = runif(n, 0, 2)
+  X = pmin(F, G)
+  D = (F < G)
+  lines(tt, sapply(tt, FKM, sample = X, delta = D), col = color)
+  legend("bottomright",col=1:6,legend=c("real", 5, 10, 100, 500, 1000), lty=1:6)
+  amount = c(0, 0)
+  for (m in 1:1000)
+  {
+    F_m = rtriangle(n, 0, 2)
+    G_m = runif(n, 0, 2)
+    X_m = pmin(F, G)
+    D_m = (F < G)
+    interval = Fconf(x_value, X_m, D_m, alpha)
+    f_x = pmin(ptriangle(x_value, 0, 2), punif(x_value, 0, 2))
+    amount[1] = amount[1] + ((interval[1] <= f_x) & (f_x <= interval[2]))
+  
+  }
+  A_freq = rbind(A_freq, amount / 1000)
+}
+colnames(A_freq) = c("1/2 for F", "1/2 for ln(1-F)")
+rownames(A_freq) = c(5, 10, 100, 500, 1000)
+A_freq 
+
+
---
+++ lab2.R
@@ -0,0 +1,66 @@
+library(triangle)
+
+omega = function(x) 1 - x/2
+#omega = function(x) 1/(1+x)
+
+Femp = function(x, sample) mean(sample < x)
+
+F_HT = function(x, sample){
+  sample_up = (sample < x) / omega(sample)
+  sample_down = 1 / omega(sample)
+  sum(sample_up) / sum(sample_down)
+}
+
+F_HT_general = function(x, sample)
+{
+  
+  
+}
+
+
+F_Vardi_general = function(sample1, sample2)
+{
+  sample = c(sample1, sample2)
+  n2 = length(sample2)
+  Lamda = function(l) abs(sum(omega(sample) / (n2*sample + l)) - 1)
+  l = nlm(Lamda, 100)$estimate
+  f = function(y) 1/(n2 * omega(y) + l)
+  w = 1 / sum(f(sample))
+  p = w * f(sample)
+  function(x)  {sum(p * (sample < x))}
+}
+
+Femp_general = function(x){
+  n = length(x)
+  x = c(-Inf, sort(x))
+  f = (0 : n) / n
+  function(t){f[findInterval(t, x)]}
+}
+
+F_mean = function(x, f_emp1, sample2) 1/2*(f_emp1(x) + F_HT(x, sample2))
+
+n1 = 1000
+n2 = 1000
+
+tt = seq(0, 2, 0.01)
+#tt = seq(0, 20, 0.01)
+X_abs = rtriangle(n1, 0, 2)
+X_test = rtriangle(n2, 0, 2)
+#X_abs = rchisq(n1, df = 3)
+#X_test = rchisq(n1, df = 3)
+X_shift = c()
+for (k in 1:n2){
+  temp = runif(1, 0, 1)
+  if (temp < omega(X_test[k]))
+      X_shift = c(X_shift, X_test[k])
+}
+Femp = Femp_general(X_abs)
+F_Vardi = F_Vardi_general(X_abs, X_shift)
+
+matplot(tt, ptriangle(tt, 0, 2), col = 1, type = "l")
+#matplot(tt, pchisq(tt, df = 3), col = 1, type = "l")
+lines(tt, sapply(tt, Femp), col = 2)
+lines(tt, sapply(tt, F_HT, sample = X_shift), col = 3)
+lines(tt, sapply(tt, F_mean, f_emp1 = Femp, sample2 = X_shift), col = 4)
+lines(tt, sapply(tt, F_Vardi), col = 5)
+legend("bottomright",col=1:5, legend=c("real","Femp", "F_HT", "1/2(Femp + F_HT)", "F_Vardi"), lty=c(1,1,1,1,1))
